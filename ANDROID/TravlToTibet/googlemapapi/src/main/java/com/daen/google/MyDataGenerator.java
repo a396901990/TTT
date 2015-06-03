1d1c3d313997a73728768ba81875b664ae2128b4
Dean <396901990@qq.com>
2015-06-03 23:56:59 +0800

    save result to file

--- ANDROID/TravlToTibet/googlemapapi/src/main/java/com/daen/google/MyDataGenerator.java
+++ ANDROID/TravlToTibet/googlemapapi/src/main/java/com/daen/google/MyDataGenerator.java
@@ -1,6 +1,8 @@
 package com.daen.google;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Created by 95 on 2015/5/29.
@@ -10,22 +12,13 @@ public class MyDataGenerator {
     public static void main(String[] args) throws Exception {
 
         final ArrayList<Geocode> geocodes = new ArrayList<Geocode>();
-        final Geocode geocode = new Geocode();
-        geocode.setName("叶城县");
-        final Geocode geocode1 = new Geocode();
-        geocode1.setName("柯克亚乡");
-        final Geocode geocode2 = new Geocode();
-        geocode2.setName("普萨村");
-        final Geocode geocode3 = new Geocode();
-        geocode3.setName("阿克美其特村");
-        final Geocode geocode4 = new Geocode();
-        geocode4.setName("库地");
 
-        geocodes.add(geocode);
-        geocodes.add(geocode1);
-        geocodes.add(geocode2);
-        geocodes.add(geocode3);
-        geocodes.add(geocode4);
+        geocodes.add(new Geocode("叶城县", 0));
+        geocodes.add(new Geocode("柯克亚乡", 62));
+        geocodes.add(new Geocode("普萨村", 72));
+        geocodes.add(new Geocode("阿克美其特村", 100, "叶城县"));
+        geocodes.add(new Geocode("库地", 160));
+        geocodes.add(new Geocode("大红柳滩", 487));
 
         final ArrayList<Geocode> geos = new ArrayList<Geocode>();
         DetailsInfoRunnable detailsInfoRunnable = new DetailsInfoRunnable(geocodes, new DetailsInfoRunnable.FetchCallback() {
@@ -33,9 +26,10 @@ public class MyDataGenerator {
             public void fetchSuccess(Geocode geocode) {
 
                 geos.add(geocode);
-
                 if (geos.size() == geocodes.size()) {
-                    System.out.print(geos);
+                    Collections.sort(geos, Geocode.MileageComparator);
+                    ParseJson.parseToFile(geos);
+                    //System.out.print(geos);
                 }
             }
 
