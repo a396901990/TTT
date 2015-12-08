4b522aca1638bf878637965e5cc7d60992c597b4
Dean Guo <396901990@qq.com>
2015-12-08 17:20:46 +0800

    redesign guide view

---
+++ ANDROID/TravlToTibet/app/src/main/java/com/dean/travltotibet/fragment/BaseGuideFragment.java
@@ -0,0 +1,134 @@
+package com.dean.travltotibet.fragment;
+
+import android.app.Fragment;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Bundle;
+
+import com.dean.travltotibet.activity.RouteActivity;
+import com.dean.travltotibet.ui.CustomProgress;
+import com.dean.travltotibet.ui.fab.FloatingActionMenu;
+
+/**
+ * Created by DeanGuo on 8/31/15.
+ */
+public abstract class BaseGuideFragment extends Fragment {
+
+    // 标志位，标志是否可见
+    private boolean isVisible = false;
+    // 标志位，标志是否初始化完成
+    private boolean isPrepared = false;
+    // 是否已被加载过一次，第二次就不再去请求数据了
+    private boolean mHasLoadedOnce = false;
+
+    private String currentPlan;
+
+    /**
+     * Fragment当前状态是否可见
+     */
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        super.setUserVisibleHint(isVisibleToUser);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1) {
+            if (getUserVisibleHint()) {
+                isVisible = true;
+            } else {
+                isVisible = false;
+            }
+        }
+
+        lazyLoad();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        isPrepared = true;
+
+        lazyLoad();
+    }
+
+    /**
+     * 延迟加载
+     * 子类必须重写此方法
+     */
+    protected void lazyLoad() {
+
+        if (!isPrepared || !isVisible || mHasLoadedOnce) {
+            return;
+        }
+
+        getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                new AsyncTask<Void, Void, Boolean>() {
+
+                    @Override
+                    protected void onPreExecute() {
+                        super.onPreExecute();
+                        // 准备加载
+                        currentPlan = ((RouteActivity) getActivity()).getPlanStart();
+                        CustomProgress.show(getActivity(), false);
+                        onLoadPrepared();
+                    }
+
+                    @Override
+                    protected Boolean doInBackground(Void... params) {
+                        try {
+                            Thread.sleep(600);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        onLoading();
+                        return true;
+                    }
+
+                    @Override
+                    protected void onPostExecute(Boolean isSuccess) {
+                        if (isSuccess) {
+                            mHasLoadedOnce = true;
+                            // 加载成功
+                            onLoadFinished();
+                            CustomProgress.dismissDialog();
+                        } else {
+                            // 加载失败
+                            CustomProgress.dismissDialog();
+                        }
+                    }
+                }.execute();
+            }
+        });
+    }
+
+    public boolean isLoaded() {
+        return isPrepared && isVisible && mHasLoadedOnce;
+    }
+
+    protected abstract void onLoadPrepared();
+
+    protected abstract void onLoading();
+
+    protected abstract void onLoadFinished();
+
+    /**
+     * 跟新fragment路线
+     */
+    public abstract void updateRoute();
+
+    /**
+     * fragment菜单按钮
+     */
+    public abstract void fabBtnEvent();
+
+    public abstract void initMenu(FloatingActionMenu menu);
+
+    public void setCurrentPlan(String currentPlan, String end) {
+        this.currentPlan = currentPlan + end;
+    }
+
+    public boolean isCurrentPlan(String planStart, String planEnd) {
+        return currentPlan.equals(planStart + planEnd);
+    }
+}
---
+++ ANDROID/TravlToTibet/app/src/main/java/com/dean/travltotibet/fragment/GuideOverViewFragment.java
@@ -0,0 +1,7 @@
+package com.dean.travltotibet.fragment;
+
+/**
+ * Created by DeanGuo on 12/8/15.
+ */
+public class GuideOverViewFragment {
+}
