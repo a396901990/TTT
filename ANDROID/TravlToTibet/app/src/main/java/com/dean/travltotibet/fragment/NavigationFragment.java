14b58d0e2f81a02bce2dd74676a4e00c8e6fea93
Dean Guo <396901990@qq.com>
2015-12-19 14:46:27 +0800

    share function

--- ANDROID/TravlToTibet/app/src/main/java/com/dean/travltotibet/fragment/NavigationFragment.java
+++ ANDROID/TravlToTibet/app/src/main/java/com/dean/travltotibet/fragment/NavigationFragment.java
@@ -19,6 +19,8 @@ import cn.bmob.v3.BmobQuery;
 import cn.bmob.v3.listener.FindCallback;
 import cn.bmob.v3.update.BmobUpdateAgent;
 import cn.bmob.v3.update.UpdateResponse;
+import cn.sharesdk.framework.ShareSDK;
+import cn.sharesdk.onekeyshare.OnekeyShare;
 
 /**
  * Created by DeanGuo on 12/09/15.
@@ -95,7 +97,7 @@ public class NavigationFragment extends Fragment {
         shareView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                share();
             }
         });
         // 关于视图
@@ -106,6 +108,34 @@ public class NavigationFragment extends Fragment {
                 startActivity(new Intent(getActivity(), AboutSettingActivity.class));
             }
         });
+    }
+
+    private void share() {
+        ShareSDK.initSDK(getActivity());
+        OnekeyShare oks = new OnekeyShare();
+        //关闭sso授权
+        oks.disableSSOWhenAuthorize();
+
+        // title标题：微信、QQ（新浪微博不需要标题）
+        oks.setTitle("我是分享标题");  //最多30个字符
+
+        // text是分享文本：所有平台都需要这个字段
+        oks.setText("我是分享文本，啦啦啦~http://uestcbmi.com/");  //最多40个字符
+
+        // imagePath是图片的本地路径：除Linked-In以外的平台都支持此参数
+        //oks.setImagePath(Environment.getExternalStorageDirectory() + "/meinv.jpg");//确保SDcard下面存在此张图片
+
+        //网络图片的url：所有平台
+        oks.setImageUrl("http://7sby7r.com1.z0.glb.clouddn.com/CYSJ_02.jpg");//网络图片rul
+
+        // url：仅在微信（包括好友和朋友圈）中使用
+        oks.setUrl("https://github.com/a396901990");   //网友点进链接后，可以看到分享的详情
+
+        // Url：仅在QQ空间使用
+        oks.setTitleUrl("https://github.com/a396901990");  //网友点进链接后，可以看到分享的详情
+
+        // 启动分享GUI
+        oks.show(getActivity());
     }
 
     public void updateVersionCheck(final Context mContext) {
